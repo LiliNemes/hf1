3257518643e47f7355cb7aee9b68bcaf246885b0
Lili Nemes <lili.nemes@outlook.com>
2023-03-26 01:06:48 +0100

    init

---
+++ cmake-build-debug/glut-prefix/tmp/glut-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/BME/grafika/hf1/src/freeglut"
+  "C:/BME/grafika/hf1/cmake-build-debug/glut-prefix/src/glut-build"
+  "C:/BME/grafika/hf1/cmake-build-debug/glut-prefix"
+  "C:/BME/grafika/hf1/cmake-build-debug/glut-prefix/tmp"
+  "C:/BME/grafika/hf1/cmake-build-debug/glut-prefix/src/glut-stamp"
+  "C:/BME/grafika/hf1/cmake-build-debug/glut-prefix/src"
+  "C:/BME/grafika/hf1/cmake-build-debug/glut-prefix/src/glut-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/BME/grafika/hf1/cmake-build-debug/glut-prefix/src/glut-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/BME/grafika/hf1/cmake-build-debug/glut-prefix/src/glut-stamp${cfgdir}") # cfgdir has leading slash
+endif()
